41d7cd820430621904bd8c475f5ac68a0b39d214
Orban <orbanlaszlo94@gmail.com>
2025-09-27 21:30:39 +0300

    Init flight projekt

---
+++ sb_flight_mvc/src/main/java/pti/sb_flight_mvc/SbFlightMvcApplication.java
@@ -0,0 +1,13 @@
+package pti.sb_flight_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbFlightMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbFlightMvcApplication.class, args);
+	}
+
+}
---
+++ sb_flight_mvc/src/test/java/pti/sb_flight_mvc/SbFlightMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package pti.sb_flight_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbFlightMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
